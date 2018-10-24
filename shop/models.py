6ae447169b10c1e292226ab247fac385de98dc3a
vonmutinda <maxwellwambua@gmail.com>
2018-10-24 10:25:39 +0300

    Goodie Class Model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Goodie ( models.Model ):
+    '''
+    Basically this is Shop Items Model
+    '''
+    name = models.CharField(max_lenght=250)
+    category = models.ForeignKey(Catalog)
+    description = models.TextField()
+    catalog_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def add_goodie(self):
+        '''
+        Add an item to shelf
+        '''
+        return self.save()
+
+    def delete_goodie(self):
+        return self.delete()
