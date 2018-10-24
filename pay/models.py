ae9e520ba54a82af394b7c8b59ad05d58521e684
vonmutinda <maxwellwambua@gmail.com>
2018-10-24 10:59:08 +0300

    customer profile model

--- pay/models.py
+++ pay/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+
+
+class Profile(models.Model):
+    '''
+    Profile of an customer . More Information .
+    '''
+    user = models.OneToOneField(User,  on_delete=models.CASCADE)
+    profile_picture = models.ImageField(
+        upload_to='static/profile', default="https://imgur.com/jVr43h8.png", blank=True)
+    about = models.TextField(max_length=100, blank=True, null=True )
+
+    def __str__(self):
+        return self.user.username
+
+    @classmethod
+    def get_user_profile(cls, user):
+        return cls.objects.get(user=user)
+
+    @classmethod
+    def get_all_profiles(cls):
+        return cls.objects.all()
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
