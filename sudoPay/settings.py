bef3f2d22c5c7a8585ac099ad217e6749babbbc0
koyoo--maxwel <koyoomaxwel@.com>
2018-11-01 10:28:27 +0300

    # solved error ModuleNotFoundError: No module named qr_code with command ---virtualenv virtual

--- sudoPay/settings.py
+++ sudoPay/settings.py
@@ -55,6 +55,8 @@ INSTALLED_APPS = [
     'pay.apps.PayConfig' ,
     'shop.apps.ShopConfig',
     'qr_code'
+    
+
 ]
 
 MIDDLEWARE = [
@@ -116,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
